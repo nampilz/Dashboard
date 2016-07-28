a33d70b44536985f67c16ec386182f52a317dee2
Nam <nguyenbui@hotmail.de>
2016-07-28 14:43:38 +0200

    Create new php files

--- Overview.php
+++ Overview.php
@@ -72,6 +72,81 @@
                     </div>
                 </div>
                 <!-- /.row -->
+                <div class="row">
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-9 text-center">
+                                        <div class="h4">CMP</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="CMP.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-green">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-9 text-center">
+                                        <div class="h4">Client Portal</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="ClientPortal.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-yellow">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-9 text-center">
+                                        <div class="h4">Service Bank</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="ServiceBank.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-red">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-9 text-center">
+                                        <div class="h4">Tosca</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="Tosca.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
             </div>
             <!-- /.container-fluid -->
 
