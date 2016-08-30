76f3b7309c1a805ab9c997808c27c191415112b5
Nam <nguyenbui@hotmail.de>
2016-08-30 17:49:43 +0200

    Create new File -> AppOverview (generic page to create the applications overview

---
+++ AppOverview.php
@@ -0,0 +1,197 @@
+<?php
+$test = $_GET["title"];
+
+$host="localhost";
+$user="root";
+$password="";
+$db = "GlobalDB";
+
+$con=mysqli_connect($host,$user,$password, $db);
+
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+$sqlGetApplicationData = "SELECT DISTINCT TESTED_ON, TESTER FROM TESTREPORTING WHERE TESTED_APPLICATION = 'CMP'";
+$res = mysqli_query($con, $sqlGetApplicationData);
+
+mysqli_close($con);
+
+function printReportTable()
+{
+    global $res;
+    while ($row = $res->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>", "<a href='ReportPageTest.php'>","CMP SPRINT 1","</a>", "</td>";
+        echo "<td>", $row['TESTED_ON'], "</td>";
+        echo "<td>", $row['TESTER'], "</td>";
+        echo "</tr>";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>QA Reporting System</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/sb-admin.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+        google.charts.load('current', {'packages':['line']});
+        google.charts.setOnLoadCallback(drawChart);
+
+        function drawChart() {
+
+            var data = new google.visualization.DataTable();
+            data.addColumn('number', 'Day');
+            data.addColumn('number', 'Passed');
+            data.addColumn('number', 'Failed');
+
+
+            data.addRows([
+                [1, 4, 0],
+                [2, 3, 1],
+                [3, 3, 1],
+                [4, 4, 0],
+                [5, 2, 2],
+                [6, 4, 0]
+            ]);
+
+            var chartTitle = "";
+            chartTitle = <?php echo json_encode($test) ?>;
+
+            var options = {
+                chart: {
+                    title: chartTitle
+                },
+                width: 1000,
+                height: 300
+            };
+
+            var chart = new google.charts.Line(document.getElementById('linechart_material'));
+
+            chart.draw(data, options);
+        }
+    </script>
+
+</head>
+
+<body>
+
+<div id="wrapper">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="Overview.php">QA Reporting System</a>
+        </div>
+
+        <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+            <ul class="nav navbar-nav side-nav">
+                <li>
+                    <a href="Overview.php"><i class="fa fa-fw fa-dashboard"></i> Overview</a>
+                </li>
+                <li>
+                    <a href="ReportPage.php"><i class="fa fa-fw fa-bar-chart-o"></i> Reports</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </nav>
+
+    <div id="page-wrapper">
+
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">
+                        <?php echo $test ?> Overview Test
+                    </h1>
+                </div>
+            </div>
+            <!-- /.row -->
+
+            <div class="row">
+                <div class="col-lg-12">
+
+                    <!--Line Chart-->
+                    <div id="linechart_material"></div>
+                    <hr>
+                </div>
+                <!-- List of test runs -->
+                <div class="col-lg-12">
+                    <h3>Reports</h3>
+                </div>
+                <div class="col-lg-12">
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-hover table-striped">
+                            <thead>
+                                <tr>
+                                    <th width="90%">Name</th>
+                                    <th width="50%">Datum</th>
+                                    <th>Tester</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            <?php
+                            printReportTable();
+                            ?>
+                            </tbody>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+        <!-- /.container-fluid -->
+
+    </div>
+    <!-- /#page-wrapper -->
+
+</div>
+<!-- /#wrapper -->
+
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
